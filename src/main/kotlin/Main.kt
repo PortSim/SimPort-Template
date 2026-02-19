a0fd86adec47327274c5832745843d60d2bc9f98
ms6123 <ms6123@imperial.ac.uk>
2026-02-19 14:41:23 +0000

    Improvement: More comprehensive demo port.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,50 +1,83 @@
 package com.example
 
+import com.group7.dsl.NodeBuilder
 import com.group7.dsl.arrivals
 import com.group7.dsl.buildScenario
+import com.group7.dsl.thenDelay
 import com.group7.dsl.thenFork
 import com.group7.dsl.thenJoin
+import com.group7.dsl.thenPushFork
 import com.group7.dsl.thenQueue
 import com.group7.dsl.thenService
 import com.group7.dsl.thenSink
-import com.group7.dsl.trackAll
+import com.group7.dsl.trackGlobal
 import com.group7.dsl.withMetrics
 import com.group7.generators.Delays
 import com.group7.generators.Generators
+import com.group7.metrics.Latency
 import com.group7.metrics.Occupancy
-import com.group7.policies.fork.ForkPolicy
-import com.group7.policies.fork.LeastFullForkPolicy
-import com.group7.policies.generic_fj.RandomPolicy
-import com.group7.policies.generic_fj.forkPolicy
-import com.group7.properties.Queue
+import com.group7.metrics.ResidenceTime
 import com.group7.runSimulations
+import com.group7.utils.thenSubnetwork
+import kotlin.time.Duration
 import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.minutes
+import kotlin.time.DurationUnit
 
 fun main() {
     runSimulations(
-        mapOf(
-            "Random" to demoPort(forkPolicy(RandomPolicy())),
-            "JSQ" to demoPort(LeastFullForkPolicy())
-        ),
-        20.days,
+        (12..18).associate { numCranes ->
+            "$numCranes Cranes" to demoPort(numCranes = numCranes)
+        },
+        100.days,
     )
 }
 
 private class Truck
 
 private fun demoPort(
-    policy: ForkPolicy<Truck>
+    entryGateLanes: Int = 6,
+    exitGateLanes: Int = 6,
+    numCranes: Int = 29,
+    truckArrivalsPerHour: Double = 50.0,
+    averageGateServiceTime: Duration = 6.minutes,
+    averageTravelTime: Duration = 5.6.minutes,
+    averageHandlingTimeAtStack: Duration = 6.minutes,
+    numTokens: Int = 30,
 ) = buildScenario {
     arrivals(
         "Truck Arrivals",
-        Generators.constant(::Truck, Delays.exponentialWithMean(3.1.minutes)),
-    ).thenFork("Truck Split", listOf({
-        it.thenQueue("Long Service Queue").thenService("Long Service", Delays.exponentialWithMean(10.minutes))
-    }, {
-        it.thenQueue("Short Service Queue").thenService("Short Service", Delays.exponentialWithMean(3.minutes))
-    }), policy)
-        .thenJoin("Truck Join")
+        Generators.constant(::Truck, Delays.exponential(truckArrivalsPerHour, DurationUnit.HOURS)),
+    )
+        .thenQueue("Truck Arrival Queue")
+        .thenSubnetwork(capacity = numTokens) { entrance ->
+            entrance
+                .thenQueueAndGates("Entrance", entryGateLanes, averageGateServiceTime)
+                .thenDelay("Travel to stacks", Delays.exponentialWithMean(averageTravelTime))
+                .thenFork("ASC Split", numCranes) { i, lane ->
+                    lane
+                        .thenQueue("ASC Queue $i")
+                        .thenService("ASC $i", Delays.exponentialWithMean(averageHandlingTimeAtStack))
+                }
+                .thenJoin("ASC Join")
+                .thenDelay("Travel to gates", Delays.exponentialWithMean(averageTravelTime))
+                .thenQueueAndGates("Exit", exitGateLanes, averageGateServiceTime)
+        }
         .thenSink("Truck Departures")
 }
-    .withMetrics { trackAll<Queue<*>>(Occupancy) }
+    .withMetrics { 
+        trackGlobal(Occupancy)
+        trackGlobal(ResidenceTime)
+        trackGlobal(Latency)
+    }
+
+private fun <T> NodeBuilder<T, *>.thenQueueAndGates(
+    description: String,
+    numLanes: Int,
+    averageServiceTime: Duration,
+) = this
+    .thenQueue("$description Queue")
+    .thenPushFork("$description Lane Split", numLanes) { i, lane ->
+        lane.thenService("$description Gate $i", Delays.exponentialWithMean(averageServiceTime))
+    }
+    .thenJoin("$description Lane Join")
